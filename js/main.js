b1e583f6c1139c64f8b1ca168f3a77fcacee2406
ZZ1227 <zengzhi1227@gmail.com>
2021-03-08 07:56:26 -0800

    added cat and changed outlines

--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ class Viewer {
             
             if (this.model) {
                 this.model.position = new PIXI.Point((width * 0.5), (height * 0.5));
-                this.model.scale = new PIXI.Point((this.model.position.x * 0.36), (this.model.position.x * 0.36));
+                this.model.scale = new PIXI.Point((this.model.position.x * 1.26), (this.model.position.x * 1.26));
                 this.model.masks.resize(this.app.view.width, this.app.view.height);
 
             }
